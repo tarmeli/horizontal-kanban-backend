b136571cb3f8483a38613a95a482fe83c28c8b05
roope <roope.pajunen@gmail.com>
2019-09-13 13:05:57 +0300

    Add resolver for tasksById

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,24 @@
+const { getUserId } = require("../utils")
+
 const info = () => 'This is the API of a Kanban poc'
 
 const tasks = async (root, args, context, info) => {
     return context.prisma.tasks()
 }
 
+const tasksById = async (root, args, context, info) => {
+    const userId = getUserId(context);
+    return context.prisma.tasks({
+      where: {
+        user: {
+          id: userId,
+        },
+      },
+    });
+  };
+
 module.exports = {
     info,
-    tasks
+    tasks,
+    tasksById,
 }
